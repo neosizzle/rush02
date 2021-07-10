c52487c799784efc0a1e13d48d87b7122de9f6fb
Dylon Tan <dtan@u81z06s01.42kl.edu.my>
2021-07-10 10:24:41 +0800

    added bst header file

---
+++ includes/ftbst.h
@@ -0,0 +1,21 @@
+#ifndef FT_BST_H
+# define FT_BST_H
+
+typedef struct s_entry {
+	int	key;
+	char	*value;
+}	t_entry;
+
+typedef struct s_node {
+	t_entry	*entry;
+	struct s_node	*left;
+	struct s_node	*right;
+}	t_node;
+
+t_node	*create_node(t_entry *entry);
+
+t_entry  *create_entry(int key, char *value);
+
+t_node	*insert_entry(t_node *root, int key, char *value);
+
+#endif
