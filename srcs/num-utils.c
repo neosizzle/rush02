aa86b87ef7cf7754ae7d9e75f0d367b8934809e9
Dylon Tan <dtan@u81z06s01.42kl.edu.my>
2021-07-10 11:59:40 +0800

    added num utils file

---
+++ srcs/num-utils.c
@@ -0,0 +1,22 @@
+int	is_whitespace(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t' || c == '\v' || c == '\r' || c == '\f')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	res;
+
+	res = 0;
+	while (is_whitespace(*str))
+		str++;
+	while (*str)
+	{
+		res *= 10;
+		res += (*str - '0');
+		str++;
+	}
+	return (res);
+}
