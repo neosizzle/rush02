e07b9390e91356f3cd5c35afc9aa22ce8763c7a5
Jie xi Lim <jielim@u81z08s03.42kl.edu.my>
2021-07-10 15:47:01 +0800

    Add conversion.h

---
+++ includes/conversion.h
@@ -0,0 +1,10 @@
+#ifndef CONVERSION_H
+
+# define CONVERSION_H
+
+void			convert_to_words(unsigned int num);
+void			put_str(char *str);
+unsigned int	get_highest_ten_mulp(unsigned int value);
+void			handle_tens(unsigned int num);
+
+#endif
