1f908dbe543f9e39156319174c26939560b29ca0
Jie xi Lim <jielim@u81z08s03.42kl.edu.my>
2021-07-11 12:54:55 +0800

    Update conversion.h and add cvrs_hlpr.h

---
+++ includes/cvrs_hlpr.h
@@ -0,0 +1,10 @@
+#ifndef CVRS_HLPR_H
+
+# define CVRS_HLPR_H
+
+void	put_str(char *str);
+void	handle_tens(unsigned int num);
+void	handle_aft_mod(unsigned int num, unsigned int ten_mulp);
+int		is_third_place(int ten_mulp);
+
+#endif
